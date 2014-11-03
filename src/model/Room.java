2c1b671a5621c9f6c3c6fc17d161a9f8f93a6327
Kaspryshyn <KaspYar@yahoo.com>
2014-11-03 12:34:09 +0200

    no message

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.util.LinkedList;
+
 public class Room {
+private LinkedList<Student> inhabitans;
+private int roomNumber;
+private TypeOfRoom capacity;
+private int free;
+private String genderType;
 
 }
--- src/model/TypeOfRoom.java
+++ src/model/TypeOfRoom.java
@@ -1,8 +1,78 @@
 package model;
 
 public abstract class TypeOfRoom {
-private int capacity;
-private int price;
 public abstract int getCapacity();
+public abstract void setPrice(int prc);
 public abstract int getPrice();
+}
+class Single extends TypeOfRoom{
+	private int capacity;
+	private int price;
+	public Single(){
+		capacity = 1;
+		price = 400;
+	}
+	public int getCapacity(){
+		return capacity;
+	}
+	public void setPrice(int prc){
+		this.price = prc;
+	}
+	public int getPrice(){
+		return this.price;
+	}
+}
+
+class Doubl extends TypeOfRoom{
+	private int capacity;
+	private int price;
+	public Doubl(){
+		capacity = 2;
+		price = 300;
+	}
+	public int getCapacity(){
+		return capacity;
+	}
+	public void setPrice(int prc){
+		this.price = prc;
+	}
+	public int getPrice(){
+		return this.price;
+	}
+}
+
+class Tripple extends TypeOfRoom{
+	private int capacity;
+	private int price;
+	public Tripple(){
+		capacity = 3;
+		price = 250;
+	}
+	public int getCapacity(){
+		return capacity;
+	}
+	public void setPrice(int prc){
+		this.price = prc;
+	}
+	public int getPrice(){
+		return this.price;
+	}
+}
+
+class Four extends TypeOfRoom{
+	private int capacity;
+	private int price;
+	public Four(){
+		capacity = 4;
+		price = 250;
+	}
+	public int getCapacity(){
+		return capacity;
+	}
+	public void setPrice(int prc){
+		this.price = prc;
+	}
+	public int getPrice(){
+		return this.price;
+	}
 }
