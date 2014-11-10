84ce7f461a28cee3acbc0553cb9657babf5ab640
Kaspryshyn <KaspYar@yahoo.com>
2014-11-10 18:31:04 +0200

    no message

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -20,64 +20,18 @@ public class Controller {
 		login.addListener(new ListenerLogIn());		
 	}
 /*
- * Here starts all Listener<Types>
- * */	
+  Here starts all Listener<Types>
+*/	
 	private class ListenerLogIn implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			Object source = e.getSource();
-			if(source == login.getSpravka()){
+			if(source == login.getBtnSpravka()){
 				System.out.println("ListenerLogIn: pressed getInfo");
 				//login.getInfoTool().setVisible(true);
 			}
-			if (source == login.getLogInBtn()){
-				System.out.println("ListenerLogIn: pressed LogIn");
-				/*
-				  String log = login.getLogin().getText();
-				
-				char[]  pswd = login.getPswd().getPassword();
-				String pswdStr = new String(pswd);
-				login.getInfoTool().setVisible(false);
-				
-				if (log.equals("") || pswdStr.equals("")){
-					login.getPswd().setText("");
-					login.getAttentLbl().setText("Перевірте правильність даних!");
-					
-					
-					
-				}
-				else if (Model.Model.INSTANCE.doLogInAdmin(log, pswdStr)){
-					adminView = new AdminView();
-					adminView.addListener(new AdminViewListener());
-					login.getLogin().setText("");
-					login.getPswd().setText("");
-					login.getAttentLbl().setText("");
-					frame.showPane(adminView);
-				} else
-					try {
-						if (Model.Model.INSTANCE.doLogInStudent(log, pswdStr)){
-							studView = new StudView("Ви увійшли як " + log);
-							studView.addListener(new StudViewListener());
-							login.getPswd().setText("");
-							login.getAttentLbl().setText("");
-							frame.showPane(studView);
-						}
-						else if (Model.Model.INSTANCE.doLogInComend(log, pswdStr)){
-							comendView = new ComendView();
-							comendView.addListener(new ComendViewListener() );
-							login.getLogin().setText("");
-							login.getPswd().setText("");
-							login.getAttentLbl().setText("");
-							frame.showPane(comendView);
-						}
-						else{
-							login.getPswd().setText("");
-							login.getAttentLbl().setText("Перевірте правильність даних!");
-						}
-					} catch (SQLException e1) {
-						e1.printStackTrace();
-					}
-					 */	
+			if (source == login.getBtnLogIn()){
+				System.out.println("ListenerLogIn: pressed LogIn");					
 			}
 		}
 
--- src/view/LogIn.java
+++ src/view/LogIn.java
@@ -22,33 +22,33 @@ import javax.swing.JList;
 import javax.swing.JScrollBar;
 
 public class LogIn extends MyPanel {
-	private JButton logInBtn;
-	private JTextField login;
-	private JPasswordField pswd;
-	private JLabel hello;
-	private JLabel infoTool;
-	private JButton spravka;
+	private JButton btnLogIn;
+	private JTextField textFieldLogin;
+	private JPasswordField textFieldPswd;
+	private JLabel lblGreeting;
+	private JLabel lblInfoTool;
+	private JButton btnSpravka;
 
 	
 
 	public JButton getLogInBtn() {
-		return logInBtn;
+		return btnLogIn;
 	}
 
 	public LogIn() {
 
-		logInBtn = new JButton("\u0423\u0432\u0456\u0439\u0442\u0438");
-		logInBtn.setBounds(350, 320, 100, 23);
-		add(logInBtn);
+		btnLogIn = new JButton("\u0423\u0432\u0456\u0439\u0442\u0438");
+		btnLogIn.setBounds(350, 320, 100, 23);
+		add(btnLogIn);
 		
-		login = new JTextField();
-		login.setBounds(350, 250, 100, 20);
-		add(login);
-		login.setColumns(10);
+		textFieldLogin = new JTextField();
+		textFieldLogin.setBounds(350, 250, 100, 20);
+		add(textFieldLogin);
+		textFieldLogin.setColumns(10);
 		
-		pswd = new JPasswordField();
-		pswd.setBounds(350, 285, 100, 20);
-		add(pswd);
+		textFieldPswd = new JPasswordField();
+		textFieldPswd.setBounds(350, 285, 100, 20);
+		add(textFieldPswd);
 		
 		JLabel label = new JLabel("\u041B\u043E\u0433\u0456\u043D:");
 		label.setBounds(294, 250, 50, 20);
@@ -58,43 +58,43 @@ public class LogIn extends MyPanel {
 		label_1.setBounds(294, 285, 50, 20);
 		add(label_1);
 		
-		hello = new JLabel("\u0423\u0432\u0456\u0439\u0434\u0456\u0442\u044C, \u0449\u043E\u0431 \u0440\u043E\u0437\u043F\u043E\u0447\u0430\u0442\u0438 \u0440\u043E\u0431\u043E\u0442\u0443");
-		hello.setForeground(Color.WHITE);
-		hello.setHorizontalAlignment(SwingConstants.CENTER);
-		hello.setFont(new Font("Tahoma", Font.PLAIN, 26));
-		hello.setBounds(152, 74, 509, 150);
-		add(hello);
+		lblGreeting = new JLabel("\u0423\u0432\u0456\u0439\u0434\u0456\u0442\u044C, \u0449\u043E\u0431 \u0440\u043E\u0437\u043F\u043E\u0447\u0430\u0442\u0438 \u0440\u043E\u0431\u043E\u0442\u0443");
+		lblGreeting.setForeground(Color.WHITE);
+		lblGreeting.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGreeting.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblGreeting.setBounds(152, 74, 509, 150);
+		add(lblGreeting);
 		
-		infoTool = new JLabel("\u042F\u043A\u0449\u043E \u0432\u0438 \u0441\u0442\u0443\u0434\u0435\u043D\u0442, \u0442\u043E \u0432\u0432\u0435\u0434\u0456\u0442\u044C \u0432 \u043F\u043E\u043B\u0435 \u041B\u043E\u0433\u0456\u043D \u0441\u0432\u043E\u0454 \u043F\u0440\u0456\u0437\u0432\u0438\u0449\u0435, \u0430 \u0432 \u043F\u043E\u043B\u0435 \u041F\u0430\u0440\u043E\u043B\u044C 11111(\u043F'\u044F\u0442\u044C \u043E\u0434\u0438\u043D\u0438\u0447\u043E\u043A) \u0442\u0430 \u043D\u0430\u0442\u0438\u0441\u043D\u0456\u0442\u044C \u0423\u0432\u0456\u0439\u0442\u0438.");
-		infoTool.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		infoTool.setBounds(55, 11, 735, 110);
-		infoTool.setVisible(false);
-		add(infoTool);
+		lblInfoTool = new JLabel("\u042F\u043A\u0449\u043E \u0432\u0438 \u0441\u0442\u0443\u0434\u0435\u043D\u0442, \u0442\u043E \u0432\u0432\u0435\u0434\u0456\u0442\u044C \u0432 \u043F\u043E\u043B\u0435 \u041B\u043E\u0433\u0456\u043D \u0441\u0432\u043E\u0454 \u043F\u0440\u0456\u0437\u0432\u0438\u0449\u0435, \u0430 \u0432 \u043F\u043E\u043B\u0435 \u041F\u0430\u0440\u043E\u043B\u044C 11111(\u043F'\u044F\u0442\u044C \u043E\u0434\u0438\u043D\u0438\u0447\u043E\u043A) \u0442\u0430 \u043D\u0430\u0442\u0438\u0441\u043D\u0456\u0442\u044C \u0423\u0432\u0456\u0439\u0442\u0438.");
+		lblInfoTool.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblInfoTool.setBounds(55, 11, 735, 110);
+		lblInfoTool.setVisible(false);
+		add(lblInfoTool);
 		
-		spravka = new JButton("\u0421\u043F\u0440\u0430\u0432\u043A\u0430");
-		spravka.setBounds(701, 11, 89, 23);
-		add(spravka);
+		btnSpravka = new JButton("\u0421\u043F\u0440\u0430\u0432\u043A\u0430");
+		btnSpravka.setBounds(701, 11, 89, 23);
+		add(btnSpravka);
 	}
 
-	public JTextField getLogin() {
-		return login;
+	public JButton getBtnLogIn() {
+		return btnLogIn;
 	}
 
-	public JLabel getInfoTool() {
-		return infoTool;
+	public JPasswordField getTextFieldPswd() {
+		return textFieldPswd;
+	}
+
+	public JLabel getLblInfoTool() {
+		return lblInfoTool;
+	}
+
+	public JButton getBtnSpravka() {
+		return btnSpravka;
 	}
 
 	public void addListener(ActionListener l) {
-		logInBtn.addActionListener(l);
-		spravka.addActionListener(l);
+		btnLogIn.addActionListener(l);
+		btnSpravka.addActionListener(l);
 
-	}
-
-	public JPasswordField getPswd() {
-		return pswd;
-	}
-
-	public JButton getSpravka() {
-		return spravka;
 	}
 }
