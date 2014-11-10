3aac15b53d141af6bad04c5b2a6e3bf98ce0a529
Kaspryshyn <KaspYar@yahoo.com>
2014-11-10 22:17:19 +0200

    no message

--- src/model/Living.java
+++ src/model/Living.java
@@ -3,40 +3,38 @@ package model;
 import java.util.Date;
 
 public class Living {
-private Date dateTo;
-private Date dateFrom;
-private Double price;
-private Integer room;
-private Settler settler;
-public Date getDateTo() {
-	return dateTo;
-}
-public void setDateTo(Date dateTo) {
-	this.dateTo = dateTo;
-}
-public Date getDateFrom() {
-	return dateFrom;
-}
-public void setDateFrom(Date dateFrom) {
-	this.dateFrom = dateFrom;
-}
-public Integer getRoom() {
-	return room;
-}
-public void setRoom(Integer room) {
-	this.room = room;
-}
-public Settler getSettler() {
-	return settler;
-}
-public void setSettler(Settler settler) {
-	this.settler = settler;
-}
-public Double getPrice() {
-	return price;
-}
-
-
-
-
+	private Date dateTo;
+	private Date dateFrom;
+	private Double price;
+	private Integer room;
+	private Settler settler;
+	
+	
+	public Date getDateTo() {
+		return dateTo;
+	}
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+	public Integer getRoom() {
+		return room;
+	}
+	public void setRoom(Integer room) {
+		this.room = room;
+	}
+	public Settler getSettler() {
+		return settler;
+	}
+	public void setSettler(Settler settler) {
+		this.settler = settler;
+	}
+	public Double getPrice() {
+		return price;
+	}
 }
