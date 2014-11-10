6de7424ff52212b2e71f53110529803f57f5a772
irazhuravel <irashuravluk@ukr.net>
2014-11-10 10:37:57 +0200

    no message

--- src/model/Room.java
+++ src/model/Room.java
@@ -8,7 +8,6 @@ private int roomNumber;
 private TypeOfRoom capacity;
 private int free;
 private String genderType;
-private String attribute;
 
 
 
@@ -18,6 +17,30 @@ public void addStudent(Student st){
 
 public void evictStudent(){
 	//
+}
+
+public int getFree() {
+	return free;
+}
+
+public void setFree(int free) {
+	this.free = free;
+}
+
+public LinkedList<Student> getInhabitans() {
+	return inhabitans;
+}
+
+public int getRoomNumber() {
+	return roomNumber;
+}
+
+public TypeOfRoom getCapacity() {
+	return capacity;
+}
+
+public String getGenderType() {
+	return genderType;
 }
 
 }
