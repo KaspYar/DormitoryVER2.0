caeaa2085c27b3a751ae00affd8d3315f7d23298
Kaspryshyn <KaspYar@yahoo.com>
2014-11-10 20:32:06 +0200

    no message

--- src/controller/ListenerLogIn.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import view.LogIn;
-
-
--- src/model/Settler.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public abstract class Settler implements IUser {
-	public abstract void pay();
-	public abstract int getMyRoom();
-	public abstract String getMyDormitory();
-	public abstract int getTypeOfRoom();
-}
