1b16f49d2350d017cbd2a309b2fbb3db71f6c817
irazhuravel <irashuravluk@ukr.net>
2014-11-10 19:38:26 +0200

    no message

--- src/model/Block.java
+++ src/model/Block.java
@@ -6,6 +6,8 @@ public class Block {
 private String blockId;
 private Student master;
 private ArrayList<Room> rooms;
+private Integer freeRooms;
+private Integer freeSpace;
 public Block(String id){
 	this.blockId = id;
 	this.rooms = new ArrayList<Room>(4); 
@@ -13,5 +15,23 @@ public Block(String id){
 public void changeMaster(Student mstr){
 	this.master = mstr;
 	return;
+}
+public Student getMaster() {
+	return master;
+}
+public void setMaster(Student master) {
+	this.master = master;
+}
+public String getBlockId() {
+	return blockId;
+}
+public ArrayList<Room> getRooms() {
+	return rooms;
+}
+public Integer getFreeRooms() {
+	return freeRooms;
+}
+public Integer getFreeSpace() {
+	return freeSpace;
 }
 }
