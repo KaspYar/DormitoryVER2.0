8ebbaf68173a90dfbcbc3da5b138686a27ba07cc
irazhuravel <irashuravluk@ukr.net>
2014-11-10 10:32:54 +0200

    no message

--- src/model/Room.java
+++ src/model/Room.java
@@ -11,10 +11,8 @@ private String genderType;
 private String attribute;
 
 
-public void getInhabitans(){
-	return inhabitans;
-}
-public void addStudent(Student){
+
+public void addStudent(Student st){
 	//
 }
 
@@ -22,20 +20,6 @@ public void evictStudent(){
 	//
 }
 
-public Integer getInhabitantBySurname(){
-	//
-}
-public void getFree(){
-	return free;
-}
-public void getCapacity(){
-	return capacity;
-}
-public void getGender(){
-	return this.genderType;
-}
-public void setGender(int gnd){
-	this.genderType =  gnd;
 }
 
 
